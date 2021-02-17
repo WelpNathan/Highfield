69cfffcf250acf10fb930d3b41394304d58acfac
Claire Storey <clairestorey30614@gmail.com>
2021-02-17 15:23:13 +0000

    profile page added

---
+++ ExamInvigilatorProject/ExamInvigilatorProject/Pages/LoggedIn/Profile.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+using System.Security.Authentication;
+
+namespace ExamInvigilatorProject.Pages
+{
+    public class profileModel : PageModel
+    {
+        public bool success = false;
+        public int attempts = 0;
+
+        private readonly ILogger<profileModel> _logger;
+
+        public profileModel(ILogger<profileModel> logger)
+        {
+            _logger = logger;
+        }
+
+       
+    }
+}

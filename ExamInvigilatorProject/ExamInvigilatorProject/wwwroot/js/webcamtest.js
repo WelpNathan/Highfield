70a3e1f1a8e234bbffd77a751007bc950b8bb057
Nathan Hall <welpnathan@gmail.com>
2021-02-18 12:40:57 +0000

    Webcamtest

--- ExamInvigilatorProject/ExamInvigilatorProject/wwwroot/js/webcamtest.js
+++ ExamInvigilatorProject/ExamInvigilatorProject/wwwroot/js/webcamtest.js
@@ -1,12 +1,54 @@
-﻿// Grab elements, create settings, etc.
-var video = document.getElementById('video');
+﻿const videoElem = document.getElementById("video");
+const logElem = document.getElementById("log");
+const startElem = document.getElementById("start");
+const stopElem = document.getElementById("stop");
 
-// Get access to the camera!
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
-    });
+// Options for getDisplayMedia()
+
+var displayMediaOptions = {
+    video: {
+        cursor: "always"
+    },
+    audio: false
+};
+
+// Set event listeners for the start and stop buttons
+startElem.addEventListener("click", function (evt) {
+    startCapture();
+}, false);
+
+stopElem.addEventListener("click", function (evt) {
+    stopCapture();
+}, false);
+
+console.log = msg => logElem.innerHTML += `${msg}<br>`;
+console.error = msg => logElem.innerHTML += `<span class="error">${msg}</span><br>`;
+console.warn = msg => logElem.innerHTML += `<span class="warn">${msg}<span><br>`;
+console.info = msg => logElem.innerHTML += `<span class="info">${msg}</span><br>`;
+
+async function startCapture() {
+    logElem.innerHTML = "";
+
+    try {
+        videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        dumpOptionsInfo();
+    } catch (err) {
+        console.error("Error: " + err);
+    }
+}
+
+function stopCapture(evt) {
+    let tracks = videoElem.srcObject.getTracks();
+
+    tracks.forEach(track => track.stop());
+    videoElem.srcObject = null;
+}
+
+function dumpOptionsInfo() {
+    const videoTrack = videoElem.srcObject.getVideoTracks()[0];
+
+    console.info("Track settings:");
+    console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
+    console.info("Track constraints:");
+    console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
 }
